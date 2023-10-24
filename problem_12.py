f46f570a00a2ab8219dac73dad9a29b9df1058c7
정 광희 <jgh991126@naver.com>
2023-10-24 18:09:48 +0900

    주석

--- problem_12.py
+++ problem_12.py
@@ -6,7 +6,7 @@ for _ in range(n):
     li.append([int(age),name])
 
 print(li) 
-li.sort(key=lambda x:x[0])
+li.sort(key=lambda x:x[0]) #sort함수는 정렬해주는 함수이며, key는 함수호출, lambda는 익명의 함수이다. 즉x[0]을 기준으로 정렬하라는 의미.
  
 for i in li:
     print(i[0],i[1])
